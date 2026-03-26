a8440246a0f92bd690b4b83fb5b675d73a14d78a
Eusebio Ngoy <banzaeusebio2@gmail.com>
2026-03-26 15:09:46 +0200

    changing codes

--- src/js/product.js
+++ src/js/product.js
@@ -1,19 +1,18 @@
-// src/js/product.js
-import { getParam } from "./utils.mjs";
-import ExternalServices from "./ExternalServices.mjs";
-import ProductDetails from "./ProductDetails.mjs";
+import { setLocalStorage } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
 
-// Create data source for tents category
-const dataSource = new ExternalServices("tents");
+const dataSource = new ProductData("tents");
 
-// Get product ID from URL query param
-const productID = getParam("product");
-
-if (!productID) {
-  // Use alert or throw instead of console.error to avoid ESLint warning
-  alert("No product ID specified in URL!");
-} else {
-  // Initialize product details page
-  const productPage = new ProductDetails(productID, dataSource);
-  productPage.init();
+function addProductToCart(product) {
+  setLocalStorage("so-cart", product);
 }
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
+
+// add listener to Add to Cart button
+document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
