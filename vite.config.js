30504036b7cbe51e8fcc806f6fc74c6e53569cfd
Eusebio Ngoy <banzaeusebio2@gmail.com>
2026-03-26 18:13:18 +0200

    fix vite path

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,22 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  // Project source folder
   root: "src/",
 
   build: {
-  outDir: "../dist",
-  rollupOptions: {
-    input: {
-      main: resolve(__dirname, "src/index.html"),
-      cart: resolve(__dirname, "src/cart/index.html"),
-      checkout: resolve(__dirname, "src/checkout/index.html"),
-      product: resolve(__dirname, "src/product_pages/index.html"),
-      products: resolve(__dirname, "src/product_listing/index.html"),
+    // Output folder relative to root
+    outDir: "../dist",
 
+    // Multiple HTML entry points
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        cart: resolve(__dirname, "src/cart/index.html"),
+        checkout: resolve(__dirname, "src/checkout/index.html"),
+        product: resolve(__dirname, "src/product_pages/index.html"),
+        products: resolve(__dirname, "src/product_listing/index.html")
+      },
     },
   },
-},
 });
